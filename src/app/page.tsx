d6932cafd4468c944453360c7a5f7c7c25a89a21
omaima <benhlima.omaima.solicode@gmail.com>
2024-11-03 12:23:49 +0100

    adding the header section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import { Header } from "@/sections/Header";
+import { Header } from "@/src/sections/Header";
 
 export default function Home() {
   return (
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,9 +2,10 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+  "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+  "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  "./src/sections/**/*.{js,ts,jsx,tsx,mdx}"
   ],
   theme: {
     screens: {
@@ -20,6 +21,10 @@ const config: Config = {
       }
     },
     extend: {
+      fontFamily: {
+        sans: 'var(--font-sans)',
+        serif:'var(--font-serif)'
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
