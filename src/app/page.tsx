25e668012da18014fb32601fa4067f65973918e2
omaima <benhlima.omaima.solicode@gmail.com>
2024-11-10 16:06:00 +0100

    add tape section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,13 @@ import Image from "next/image";
 import { Header } from "@/src/sections/Header";
 import { HeroSection } from "@/src/sections/Hero";
 import { ProjectsSection } from "@/src/sections/Projects";
+import { TapeSection } from "../sections/Tape";
 
 export default function Home() {
   return <div>
       <Header />
       <HeroSection />
       <ProjectsSection />
+      <TapeSection />
     </div>;
 }
