18a0771d1393879ad8978807cb8afe5c43300677
omaima <benhlima.omaima.solicode@gmail.com>
2024-11-23 01:18:43 +0100

    add the footer section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import { ProjectsSection } from "@/src/sections/Projects";
 import { TapeSection } from "../sections/Tape";
 import { AboutSection } from "../sections/About";
 import { ContactSection } from "../sections/Contact";
+import { Footer } from "../sections/Footer";
 
 export default function Home() {
   return <div>
@@ -13,6 +14,7 @@ export default function Home() {
       <ProjectsSection />
     <TapeSection />
     <AboutSection />
-    <ContactSection/>
+    <ContactSection />
+    <Footer/>
     </div>;
 }
--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,3 +1,27 @@
+import ArrowUpRight from '@/src/assets/icons/arrow-up-right.svg';
 export const Footer = () => {
-  return <div>Footer Section</div>;
+  return <footer>
+      <div className="container">
+        <div className="border-white/15 border-t  py-6 text-sm flex flex-col items-center ">
+          <div className="text-white/40 mb-8">&copy;2024.All rights reserved</div>
+          <nav className='flex flex-col items-center gap-8'>
+            <a href="#" className="inline-flex items-center gap-1.5">
+              <span className="font-semibold">
+                LinkedIn
+              </span> <ArrowUpRight className="size-4" />
+            </a>
+            <a href="#" className="inline-flex items-center gap-1.5">
+              <span className="font-semibold">
+                GitHub
+              </span> <ArrowUpRight className="size-4" />
+            </a>
+            <a href="#" className="inline-flex items-center gap-1.5">
+              <span className="font-semibold">
+                Portfolio
+              </span> <ArrowUpRight className="size-4" />
+            </a>
+          </nav>
+        </div>
+      </div>
+    </footer>;
 };
